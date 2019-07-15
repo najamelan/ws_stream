b828d632de309b12890ca9afa35ae2e36a4f6d0f
Naja Melan <najamelan@autistici.org>
2019-07-15 21:19:45 +0200

    add an echo example

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,60 +1,64 @@
 #![ feature( async_await ) ]
 
 
+// This is an echo server that returns all incoming bytes, without framing. It is used for the tests in
+// ws_stream_wasm.
+
+
+use
+{
+	ws_stream     :: { *                       } ,
+	async_runtime :: { rt, RtConfig            } ,
+	futures       :: { StreamExt, AsyncReadExt } ,
+	std           :: { io                      } ,
+};
+
+
 fn main()
 {
-	use
-	{
-		ws_stream     :: { *                 } ,
-		async_runtime :: { rt, RtConfig      } ,
-		futures       :: { StreamExt, SinkExt } ,
-		futures       :: { compat::{ Stream01CompatExt, Sink01CompatExt } } ,
-		futures_01    :: { stream::Stream as _ } ,
-		tokio         :: { codec::{ LinesCodec, Decoder } } ,
-		log           :: { * } ,
-	};
-
-	flexi_logger::Logger::with_str( "events=trace, framed=trace, wasm_websocket_stream=trace, tokio=warn" ).start().unwrap();
+	// flexi_logger::Logger::with_str( "echo=trace, ws_stream=trace, tokio=warn" ).start().unwrap();
 
 	rt::init( RtConfig::Local ).expect( "init rt" );
 
-	let server = async move
+
+	let server = async
 	{
-		debug!( "server task" );
+		println!( "server task listening at: 127.0.0.1:3212"  );
 
-		let mut connections = WsStream::listen( "127.0.0.1:3112" ).take(1);
-		let     server      = connections.next().await.expect( "1 connection" ).expect( "1 connection" ).await.expect( "WS handshake" );
+		let mut connections = WsStream::listen( "127.0.0.1:3212" );
 
-		let codec = LinesCodec::new();
+		while let Some( stream ) = connections.next().await
+		{
+			let conn = async move
+			{
+				let ws_stream = stream.expect( "tcp stream" ).await.expect( "ws handshake" );
+				println!( "Incoming connection from: {}", ws_stream.peer_addr().expect( "peer addr" ) );
 
-		let (sink, stream) = codec.framed( server ).split();
-		let mut sink       = sink.sink_compat();
-		let _stream        = stream.compat();
+				let (reader, mut writer) = ws_stream.split();
 
-		sink.send( "A line".to_string() ).await.expect( "Send a line" );
+				match reader.copy_into( &mut writer ).await
+				{
+					Ok(_) => {},
+					Err(e) => match e.kind()
+					{
+						// This can happen in the flush, but it's becaue the client has already disconnected
+						//
+						io::ErrorKind::ConnectionAborted => {}
+
+						// Other errors we want to know about
+						//
+						_ => { panic!( e ) }
+					}
+				}
+			};
+
+
+			rt::spawn( conn ).expect( "spawn conn" );
+		}
 	};
 
-	let client = async move
-	{
-		debug!( "client task" );
-
-		let client = WsStream::connect( "127.0.0.1:3112" ).await.expect( "connect to websocket" );
-
-		let codec = LinesCodec::new();
-
-		let (sink, stream) = codec.framed( client ).split();
-		let _sink          = sink.sink_compat();
-		let mut stream     = stream.compat();
-
-		let res = stream.next().await.expect( "Receive a line" ).expect( "Receive a line" );
-
-		dbg!( &res );
-
-		assert_eq!( "A line".to_string(), res );
-	};
 
 	rt::spawn( server ).expect( "spawn task" );
-	rt::spawn( client ).expect( "spawn task" );
 	rt::run();
 }
 
