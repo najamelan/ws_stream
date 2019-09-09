0479676c4d640e223e54d462eabb1fdf46a8fb34
Naja Melan <najamelan@autistici.org>
2019-09-09 21:30:37 +0200

    include readme.md for cargo doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,29 @@
-//! Provide AsyncRead/AsyncWrite over WebSockets (both tokio and 0.3)
+// See: https://github.com/rust-lang/rust/issues/44732#issuecomment-488766871
+//
+#![ cfg_attr( feature = "external_doc", feature(external_doc)         ) ]
+#![ cfg_attr( feature = "external_doc", doc(include = "../README.md") ) ]
 //!
+
+
 #![ doc    ( html_root_url = "https://docs.rs/ws_stream" ) ]
 #![ deny   ( missing_docs                                ) ]
 #![ forbid ( unsafe_code                                 ) ]
-#![ allow  ( clippy::suspicious_else_formatting, unused_imports ) ]
+#![ allow  ( clippy::suspicious_else_formatting          ) ]
 
+#![ warn
+(
+	missing_debug_implementations ,
+	missing_docs                  ,
+	nonstandard_style             ,
+	rust_2018_idioms              ,
+	trivial_casts                 ,
+	trivial_numeric_casts         ,
+	unused_extern_crates          ,
+	unused_qualifications         ,
+	single_use_lifetimes          ,
+	unreachable_pub               ,
+	variant_size_differences      ,
+)]
 
 
 #[cfg( feature = "tokio-tungstenite" )] mod handshake  ;
