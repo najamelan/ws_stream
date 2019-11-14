eed16059d6b3a56c5b6721b06f51bd284ee72732
Naja Melan <najamelan@autistici.org>
2019-11-14 14:41:06 +0100

    Write a real readme referring to the split crates.

--- src/providers.rs
+++
@@ -1,5 +0,0 @@
-#[cfg( feature = "tokio-tungstenite" )] mod tung_websocket;
-#[cfg( feature = "tokio-tungstenite" )] pub use tung_websocket::*;
-
-#[cfg( feature = "warp" )] mod warp_websocket;
-#[cfg( feature = "warp" )] pub use warp_websocket::*;
